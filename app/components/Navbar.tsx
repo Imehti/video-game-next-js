1d5c12ade397c8a622064a852e0eaa5ea344b3a0
Mahdi <khajavim497@gmail.com>
2024-05-31 15:02:57 +0430

    make navbar for home page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+// import SearchBox from "./SearchBox";
+import logo from '../../public/logo.png'
+function Navbar() {
+  return (
+    <>
+      {/* logo */}
+      <div className="flex items-center">
+        <img className="m-3 p-1 mr-1" src={logo.src} alt="logo" />
+        <h1 className="text-blue m-6 ml-1 font-mono text-2xl">Game</h1>
+        <span className="border-r border-white h-8 sm:-ml-2 -ml-4"></span>
+        <h1 className="text-white sm:ml-4 ml-1">Explore</h1>
+        <h1 className="text-white sm:ml-4 ml-2">Blog</h1>
+        <div className="sm:ml-8 ml-2">
+        {/* <SearchBox /> */}
+        </div>
+      </div>
+      {/* end logo */}
+    </>
+  );
+}
+
+export default Navbar;
