e6afab1cac3470e50d8ce4824474262a86d51e43
Mahdi <khajavim497@gmail.com>
2024-05-31 15:10:21 +0430

    add cursor for blog and explore

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,8 +8,8 @@ function Navbar() {
         <img className="m-3 p-1 mr-1" src={logo.src} alt="logo" />
         <h1 className="text-blue m-6 ml-1 font-mono text-2xl">Game</h1>
         <span className="border-r border-white h-8 sm:-ml-2 -ml-4"></span>
-        <h1 className="text-white sm:ml-4 ml-1">Explore</h1>
-        <h1 className="text-white sm:ml-4 ml-2">Blog</h1>
+        <h1 className="text-white sm:ml-4 ml-1 cursor-pointer">Explore</h1>
+        <h1 className="text-white sm:ml-4 ml-2 cursor-pointer">Blog</h1>
         <div className="sm:ml-8 ml-2">
         {/* <SearchBox /> */}
         </div>
