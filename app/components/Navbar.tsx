23e4cd04d80e63057226fd358c8d355927c30aa3
Mahdi <khajavim497@gmail.com>
2024-05-31 15:20:43 +0430

    add Link to set routes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
 // import SearchBox from "./SearchBox";
-import logo from '../../public/logo.png'
+import logo from "../../public/logo.png";
 function Navbar() {
   return (
     <>
@@ -8,11 +9,19 @@ function Navbar() {
         <img className="m-3 p-1 mr-1" src={logo.src} alt="logo" />
         <h1 className="text-blue m-6 ml-1 font-mono text-2xl">Game</h1>
         <span className="border-r border-white h-8 sm:-ml-2 -ml-4"></span>
-        <h1 className="text-white sm:ml-4 ml-1 cursor-pointer hover:text-blue hover:tracking-wider hover:font-bold">Explore</h1>
-        <h1 className="text-white sm:ml-4 ml-2 cursor-pointer hover:text-blue hover:tracking-wider hover:font-bold">Blog</h1>
-        <div className="sm:ml-8 ml-2">
-        {/* <SearchBox /> */}
-        </div>
+        <Link
+          href={"./explore"}
+          className="text-white sm:ml-4 ml-1 cursor-pointer hover:text-blue hover:tracking-wider hover:font-bold"
+        >
+          Explore
+        </Link>
+        <Link
+          href={"./blog"}
+          className="text-white sm:ml-4 ml-2 cursor-pointer hover:text-blue hover:tracking-wider hover:font-bold"
+        >
+          Blog
+        </Link>
+        <div className="sm:ml-8 ml-2">{/* <SearchBox /> */}</div>
       </div>
       {/* end logo */}
     </>
